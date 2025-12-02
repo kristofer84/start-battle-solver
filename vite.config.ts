84e1d6aba69acb343b64303af4ded4e01adfcd8c
Kristofer Nilsson <kristofer.john.nilsson@gmail.com>
2025-12-02 16:39:23 +0100

    Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // Vite config; for GitHub Pages you may want to set `base` to your repo name.
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.VITE_BASE_PATH || '/',
 });
 
 
